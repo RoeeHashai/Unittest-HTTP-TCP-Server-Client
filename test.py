6c3bbb28d0d9a73810319d0a5d16c9587b8a5ba1
Roee Hashai <roee.hashai@gmail.com>
2024-12-24 18:13:56 +0200

    test4

--- test.py
+++ test.py
@@ -96,7 +96,7 @@ class TestServerClientInteraction(unittest.TestCase):
         
     def test4(self):
         """Test checks differnt requests of files to the server one after the other"""
-        list_files = ['/index.html', 'index.html','/a/b/ref.html','/c/footube.css','c/Footube.html','/c/footube.js','/result.html', '/', '', '   ']
+        list_files = ['/index.html', 'index.html','/a/b/ref.html','/c/footube.css','c/Footube.html','/c/footube.js','/result.html', '/', '']
         for path in list_files:
             response = self.send_request_and_receive_response(path)
             self.assertIn('HTTP/1.1 200 OK', response, f"[TEST 4] did not return correctly for {path}")
